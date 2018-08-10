5330e6c2679f9b4c098d73f0cbcab2c980be9b88
Ania F <38281107+ZaNa02@users.noreply.github.com>
2018-08-10 14:03:51 +0200

    modified test js

--- utils/utils.test.js
+++ utils/utils.test.js
@@ -5,4 +5,13 @@ it('should add two numbers', () => {
 
   if (res !== 44) {
     throw new Error(`Expected 44, but got ${res}.`)
+  }
 });
+
+it('Should multiply two numbers', () => {
+  var res = utils.square(6);
+
+  if (res !== 36) {
+    throw new Error(`Expected 36, but got ${res}.`);
+    }
+  });
